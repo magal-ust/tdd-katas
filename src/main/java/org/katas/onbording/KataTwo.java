56c9de56b143fe5e0ea7e2949e7d9d33be6bd196
Maksim Galichenko <maksim.galichenko@waveaccess.ru>
2024-07-11 12:00:58 +0300

    kata2 - added validation on end of line separator

--- src/main/java/org/katas/onbording/KataTwo.java
+++ src/main/java/org/katas/onbording/KataTwo.java
@@ -3,14 +3,25 @@ package org.katas.onbording;
 
 public class KataTwo {
     public static int add(String numbers) {
+        final String sep1 = ",";
+        final String sep2 = "\n";
         int sum = 0;
+        String errors = "";
         if (numbers == null || numbers.isEmpty() || numbers.isBlank()) {
             return sum;
         }
-        String[] nums = numbers.split(",|\\\n");
+        if (numbers.endsWith(sep1) || numbers.endsWith(sep2)) {
+            errors += "Invalid input line";
+        }
+
+        String[] nums = numbers.split(sep1 + "|\\" + sep2);
         for (int i = 0; i < nums.length; i++) {
             sum += Integer.parseInt(nums[i]);
         }
+        if (!errors.isEmpty() && !errors.isBlank()) {
+            throw new IllegalArgumentException(errors);
+        }
+
         return sum;
     }
 }
