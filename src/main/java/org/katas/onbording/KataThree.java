fbb4bf31eec2cd9a75a8be7396068121309fe4e5
Maksim Galichenko <maksim.galichenko@waveaccess.ru>
2024-07-17 12:29:42 +0300

    kata3 - implement validation for password length

--- src/main/java/org/katas/onbording/KataThree.java
+++ src/main/java/org/katas/onbording/KataThree.java
@@ -3,6 +3,9 @@ package org.katas.onbording;
 
 public class KataThree {
     public static boolean validatePassword(String password) {
+        if (password == null || password.length() < 8) {
+            throw  new IllegalArgumentException("Password must be at least 8 characters");
+        }
         return true;
     }
 
