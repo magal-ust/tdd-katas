5a08692244ae9cf975e8734c36cecae1713963a4
Maksim Galichenko <maksim.galichenko@waveaccess.ru>
2024-07-17 12:38:17 +0300

    kata3 - implement digits validations for previous specification

--- src/main/java/org/katas/onbording/KataThree.java
+++ src/main/java/org/katas/onbording/KataThree.java
@@ -6,8 +6,17 @@ public class KataThree {
         if (password == null || password.length() < 8) {
             throw  new IllegalArgumentException("Password must be at least 8 characters");
         }
+
+        if (!checkDigits(password)) {
+            throw  new IllegalArgumentException("The password must contain at least 2 numbers");
+        }
+
         return true;
     }
 
+    private static boolean checkDigits(String password) {
+        String result = password.replaceAll("\\d", "");
+        return password.length() - result.length() >= 2;
+    }
 
 }
