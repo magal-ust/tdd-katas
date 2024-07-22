5a0bf936ad46df5a8a8d5207d50459afe9c40cea
Maksim Galichenko <maksim.galichenko@waveaccess.ru>
2024-07-22 13:20:14 +0300

    kata4 - implemented basic logic

--- src/main/java/org/katas/onbording/KataFour.java
+++ src/main/java/org/katas/onbording/KataFour.java
@@ -1,13 +1,20 @@
 package org.katas.onbording;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class KataFour {
     private static final String[] data = new String [] {"Paris", "Budapest","Skopje", "Rotterdam", "Valencia", "Vancouver", "Amsterdam", "Vienna", "Sydney", "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul"};
 
     public static List<String> searchBy(String query) {
         final List<String> result = new ArrayList<>();
+        Arrays.stream(data).forEach(city -> {
+            if (city.toLowerCase(Locale.ROOT).contains(query.toLowerCase(Locale.ROOT))){
+                result.add(city);
+            }
+        });
         return result;
     }
 }
