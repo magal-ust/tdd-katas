a3e03a32ba4b14a94cff62af6861534f0d28737e
Maksim Galichenko <maksim.galichenko@waveaccess.ru>
2024-07-22 13:31:35 +0300

    kata4 - implemented logic by prefix search

--- src/main/java/org/katas/onbording/KataFour.java
+++ src/main/java/org/katas/onbording/KataFour.java
@@ -10,8 +10,12 @@ public class KataFour {
 
     public static List<String> searchBy(String query) {
         final List<String> result = new ArrayList<>();
+        if (query == null || query.length() < 2) {
+            return result;
+        }
+
         Arrays.stream(data).forEach(city -> {
-            if (city.toLowerCase(Locale.ROOT).contains(query.toLowerCase(Locale.ROOT))){
+            if (city.startsWith(query)){
                 result.add(city);
             }
         });
